657dc17aec2cd013d7b1a7b0724bf59a1ba8bbca
Pocjoi <potatocrisps143@gmail.com>
2024-02-16 09:15:11 -0800

    idk wut im doin

--- 1-flattening.js
+++ 1-flattening.js
@@ -1,5 +1,11 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
-
-
+function reduce(arrays, combine, start) {
+    let current = start;
+    for (let element of arrays) {
+        current=combine(current,element);
+    }
+    return current;
+}
+console.log(reduce([1, 2, 3], [4, 5], [6]))
 // → [1, 2, 3, 4, 5, 6]
